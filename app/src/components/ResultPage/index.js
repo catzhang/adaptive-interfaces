6ead9e0c34dbc5d4329be270c92d0bab715c0549
Julie Wang <douzhang@DNa1cc6f5.SUNet>
2023-03-04 17:57:32 -0800

    connected form with results page, added calculations

--- app/src/components/ResultPage/index.js
+++ app/src/components/ResultPage/index.js
@@ -2,13 +2,18 @@ import * as React from 'react';
 import { useLocation } from 'react-router-dom';
 import FlatLayout from '../FlatLayout';
 import MixedLayout from '../MixedLayout';
+import { calculateComplexityBucket } from '../../utils/DemographicCalculations';
+
 
 function ResultPage(props) {
   const { state } = useLocation();
   console.log(state);
-  const {notes, complexity, hierarchy} = props;
+  const complexity = calculateComplexityBucket(state.age, state.country);
+  console.log(complexity);
+  const {notes, hierarchy} = props;
 
   let component = <FlatLayout notes={notes} complexity={complexity}/>; // flat
+
 
   if (hierarchy === "Mixed") {
     component = <MixedLayout notes={notes} complexity={complexity}/>;
--- app/src/utils/DemographicCalculations.js
+++ app/src/utils/DemographicCalculations.js
@@ -12,7 +12,7 @@ const BUCKETS = {
     HIGH: 3,
  }
 
-const calculateAgeComplexityBucket = (age) => {
+export const calculateAgeComplexityBucket = (age) => {
     if (age <= 40) {
         return BUCKETS.MEDIUM;
     } else {
@@ -20,6 +20,26 @@ const calculateAgeComplexityBucket = (age) => {
     }
 };
 
-// const calculateCountryComplexityBucket = (country) => {
-//     const complexity = 
-// }
+export const calculateCountryComplexityBucket = (countryIndex) => {
+    const complexity = COUNTRIES[countryIndex].complexity;
+    if (complexity <= 3.9) {
+        return BUCKETS.LOW;
+    } else if (complexity <= 4.28) {
+        return BUCKETS.MEDIUM;
+    } else {
+        return BUCKETS.HIGH;
+    }
+}
+
+export const calculateComplexityBucket = (age, countryIndex) => {
+    const ageBucket = calculateAgeComplexityBucket(age);
+    const countryBucket = calculateCountryComplexityBucket(countryIndex);
+    const score = Math.floor(0.5 * ageBucket + 0.5 * countryBucket);
+    if (score === 1) {
+        return "Low";
+    } else if (score === 2) {
+        return "Medium";
+    } else {
+        return "High";
+    }
+}
