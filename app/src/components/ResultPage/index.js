418bc9487cefb83e944e819030a82950735493ea
catzhang <cathy2zhang@gmail.com>
2023-03-10 00:23:17 -0800

    add color calculations

--- app/src/components/ResultPage/index.js
+++ app/src/components/ResultPage/index.js
@@ -3,11 +3,12 @@ import { useLocation } from 'react-router-dom';
 import FlatLayout from '../FlatLayout';
 import MixedLayout from '../MixedLayout';
 import TreeLayout from '../TreeLayout';
-import { calculateComplexityBucket, calculateHierarchy } from '../../utils/DemographicCalculations';
+import { calculateColorBucket, calculateComplexityBucket, calculateHierarchy } from '../../utils/DemographicCalculations';
 
 function ResultPage(props) {
   const { state } = useLocation();
   console.log(state);
+  const color = calculateColorBucket(state.age, state.country);
   const complexity = calculateComplexityBucket(state.age, state.country);
   console.log(complexity);
   const hierarchy = calculateHierarchy(state.country); 
--- app/src/utils/DemographicCalculations.js
+++ app/src/utils/DemographicCalculations.js
@@ -4,7 +4,43 @@ const BUCKETS = {
     LOW: 1,
     MEDIUM: 2,
     HIGH: 3,
- }
+}
+
+const calculateAgeColorBucket = (age) => {
+    if (age <= 20) {
+        return BUCKETS.HIGH;
+    } else if (age <= 30) {
+        return BUCKETS.MEDIUM;
+    } else if (age <= 50) {
+        return BUCKETS.LOW;
+    } else {
+        return BUCKETS.HIGH;
+    }
+}
+
+const calculateCountryColorBucket = (countryIndex) => {
+    const color = COUNTRIES[countryIndex].color;
+    if (color <= 5.11) {
+        return BUCKETS.LOW;
+    } else if (color <= 5.93) {
+        return BUCKETS.MEDIUM;
+    } else {
+        return BUCKETS.HIGH;
+    }
+}
+
+export const calculateColorBucket = (age, countryIndex) => {
+    const ageBucket = calculateAgeColorBucket(age);
+    const countryBucket = calculateCountryColorBucket(countryIndex);
+    const score = Math.floor(0.5 * ageBucket + 0.5 * countryBucket);
+    if (score === 1) {
+        return "Low";
+    } else if (score === 2) {
+        return "Medium";
+    } else {
+        return "High";
+    }
+}
 
 export const calculateAgeComplexityBucket = (age) => {
     if (age <= 40) {
