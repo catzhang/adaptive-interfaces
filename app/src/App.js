c0f80b503a3d636789c66766fdbf65c2eb236119
catzhang <cathy2zhang@gmail.com>
2023-03-10 21:49:52 -0800

    fix selectedNote bug

--- app/src/App.js
+++ app/src/App.js
@@ -11,36 +11,36 @@ function App() {
   // --------------- TEMPORARY LOCATION OF NOTES STATES + ENUMS ---------------------------------------------
   const [notes, setNotes] = React.useState([
     {
-        id: 1, // TODO: field must be unique
-        title: "Lorem",
-        content: "Lorem ipsum dolor sit amet. Et ipsum adipisci ab totam tenetur sit reprehenderit fuga qui harum nesciunt",
-        created_date: new Date(2023, 2, 2),
-        last_edit: '11:31 AM',
-        category: "Work"
-    },
-    {
-      id: 2, // TODO: field must be unique
-      title: "Ipsum",
+      id: 0, // TODO: field must be unique
+      title: "Personal 3/1/2023",
       content: "Lorem ipsum dolor sit amet. Et ipsum adipisci ab totam tenetur sit reprehenderit fuga qui harum nesciunt",
       created_date: new Date(2023, 2, 1),
       last_edit: '01:49 AM',
       category: "Personal"
     },
     {
-      id: 3, // TODO: field must be unique
-      title: "Lorem",
+      id: 1, // TODO: field must be unique
+      title: "Work 3/2/2023",
       content: "Lorem ipsum dolor sit amet. Et ipsum adipisci ab totam tenetur sit reprehenderit fuga qui harum nesciunt",
-      created_date: new Date(2023, 2, 3),
-      last_edit: '12:57 AM',
+      created_date: new Date(2023, 2, 2),
+      last_edit: '11:31 AM',
       category: "Work"
     },
     {
-      id: 4, // TODO: field must be unique
-      title: "Ipsum",
+      id: 2, // TODO: field must be unique
+      title: "Personal 3/2/2023",
       content: "Lorem ipsum dolor sit amet. Et ipsum adipisci ab totam tenetur sit reprehenderit fuga qui harum nesciunt",
       created_date: new Date(2023, 2, 2),
       last_edit: '11:10 PM',
       category: "Personal"
+    },
+    {
+      id: 3, // TODO: field must be unique
+      title: "Work 3/3/2023",
+      content: "Lorem ipsum dolor sit amet. Et ipsum adipisci ab totam tenetur sit reprehenderit fuga qui harum nesciunt",
+      created_date: new Date(2023, 2, 3),
+      last_edit: '12:57 AM',
+      category: "Work"
     },
   ]);
   const [complexity, setComplexity] = React.useState('');
